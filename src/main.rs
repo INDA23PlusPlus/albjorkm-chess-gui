cf5b8bd4b2fda37578cf729761d2241fd3ac499d
AB <ab3.git2@eit.se>
2023-09-28 01:52:52 +0200

    GUI for chess

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+// The boilerplate code is shamelessly stolen from the imgui rust examples.
+// If you get a linking error, download SDL2.lib from the SDL website.
+
 use glow::HasContext;
-use imgui::Context;
+use imgui::{Context, WindowFlags};
 use imgui_glow_renderer::AutoRenderer;
 use imgui_sdl2_support::SdlPlatform;
 use sdl2::{
@@ -7,93 +10,149 @@ use sdl2::{
     video::{GLProfile, Window},
 };
 
-// If you get a linking error, download SDL2.lib from the SDL website.
+struct GameState {
+    chess_board: chess::ChessBoard,
+    chess_representation: [(i8, i8); 64],
+    moving_piece: usize,
+    is_white_turn: bool,
+}
 
-// Create a new glow context.
+impl GameState {
+    fn new_game() -> GameState {
+        let chess_board = chess::ChessBoard::new();
+        let chess_representation = chess_board.get_board();
+
+        GameState {
+            chess_board,
+            chess_representation,
+            moving_piece: 65,
+            is_white_turn: true,
+        }
+    }
+    fn do_move(self: &mut Self, from: usize, to: usize) {
+        self.chess_board.move_by_index(from, to);
+        self.chess_representation = self.chess_board.get_board();
+        self.is_white_turn = !self.is_white_turn;
+    }
+}
+
+fn piece_to_unicode(piece: i8) -> &'static str {
+    // We force imgui to give our buttons an ID of 0 (which is then
+    // hashed with the square number later.
+    match piece {
+        0 => "###0",
+        1 => "\u{265F}###0",
+        2 => "\u{265C}###0",
+        3 => "\u{265E}###0",
+        4 => "\u{265D}###0",
+        5 => "\u{265B}###0",
+        6 => "\u{265A}###0",
+        _ => "###0",
+    }
+}
+
+fn team_to_color(team: i8) -> [f32; 4] {
+    if team == 1 { [1.0, 0.0, 1.0, 1.0 ] } else { [1.0, 1.0, 0.0, 1.0] }
+}
+
+fn draw_chess(ui: &imgui::Ui, game_state: &mut GameState) {
+    let display_size = ui.io().display_size;
+    let cell_size = (display_size[0].min(display_size[1]) / 8.).round() - 10.;
+    let draw_list = ui.get_window_draw_list();
+    draw_list.add_rect([0., 0.], [cell_size * 8., cell_size * 8.], 0xFF664488).filled(true).build();
+    for i in 0..64 {
+        let row = i >> 3;
+        let column = (i & 7) as f32;
+        let begin = [cell_size * column, cell_size * (row as f32)];
+        let end = [cell_size * (column + 1.), cell_size * ((row + 1) as f32)];
+        if (i+row) % 2 == 1 {
+            draw_list.add_rect(begin, end, 0xFF336622).filled(true).build();
+        }
+    }
+
+    let _no_padding = ui.push_style_var(imgui::StyleVar::ItemSpacing([0., 0.]));
+    let _no_bg = ui.push_style_color(imgui::StyleColor::Button, [0., 0., 0., 0.]);
+    let _no_border_popup = ui.push_style_var(imgui::StyleVar::PopupBorderSize(0.));
+    let _no_bg = ui.push_style_color(imgui::StyleColor::PopupBg, [0., 0., 0., 0.]);
+    for i in 0..64 {
+        if i % 8 != 0 {
+            ui.same_line();
+        }
+        let _id = ui.push_id_usize(i);
+        let (piece, team) = game_state.chess_representation[i];
+        let piece_unicode = if game_state.moving_piece == i {
+            "###0"
+        } else {
+            piece_to_unicode(piece)
+        };
+        let fg_color = team_to_color(team);
+        let _color_stck = ui.push_style_color(imgui::StyleColor::Text, fg_color);
+        ui.button_with_size(piece_unicode, [cell_size, cell_size]);
+
+        if let Some(_) = ui.drag_drop_source_config("move").begin_payload(i) {
+            game_state.moving_piece = i;
+        }
+        if let Some(v) = ui.drag_drop_target() {
+            if let Some(p) = v.accept_payload("move", imgui::DragDropFlags::empty()) {
+                if let Ok(v) = p {
+                    let source: usize = v.data;
+                    game_state.do_move(source, i);
+                }
+            }
+        }
+    }
+    if game_state.moving_piece < 65 {
+        let (piece, team) = game_state.chess_representation[game_state.moving_piece];
+        if piece != 0 {
+            let fg_color = team_to_color(team);
+            let _color_stck = ui.push_style_color(imgui::StyleColor::Text, fg_color);
+            let mouse_pos = ui.io().mouse_pos;
+            draw_list.add_text([mouse_pos[0] - 16., mouse_pos[1] - 16.], fg_color, &piece_to_unicode(piece)[0..3]);
+        }
+        if !ui.is_mouse_dragging(imgui::MouseButton::Left) {
+            game_state.moving_piece = 65;
+        }
+    }
+    if game_state.chess_board.game_end {
+        draw_list.add_text([cell_size * 4. - 150., cell_size * 4.], 0xFFFFFFFF, "IT'S SO OVER!");
+        if ui.button("Restart") {
+            *game_state = GameState::new_game();
+        }
+    } else {
+        let t = if game_state.is_white_turn {
+            "It is white's turn"
+        } else {
+            "It is black's turn"
+        };
+        ui.text(t);
+    }
+}
+
+fn draw_ui(ui: &imgui::Ui, game_state: &mut GameState) {
+    let window_token = ui.window("Chess")
+        .flags(WindowFlags::NO_DECORATION | WindowFlags::NO_BACKGROUND)
+        .position([0., 0.], imgui::Condition::Always)
+        .size(ui.io().display_size, imgui::Condition::Always).begin();
+    if let Some(_t) = window_token {
+        draw_chess(ui, game_state);
+    }
+}
+
 fn glow_context(window: &Window) -> glow::Context {
     unsafe {
         glow::Context::from_loader_function(|s| window.subsystem().gl_get_proc_address(s) as _)
     }
 }
 
-fn draw_chess(ui: &imgui::Ui) {
-    /*if let Some(_) = ui.begin_table_with_sizing("Chess", 8, imgui::TableFlags::empty(), [720., 720.], 720.) {
-        // we have to call next_row because we didn't make headers..
-        ui.table_next_column();
-        for i in 0..64 {
-            let _id = ui.push_id_usize(i);
-            ui.button_with_size("Btn", [32., 32.0]);
-            
-            if let Some(v) = ui.drag_drop_source_config("DRAG").begin_payload("Payload") {
-                ui.text("Sending text");
-                v.end()
-            }
-            if let Some(v) = ui.drag_drop_target() {
-                if let Some(p) = v.accept_payload("DRAG", imgui::DragDropFlags::empty()) {
-                    if let Ok(v) = p {
-                        let s: &str = v.data;
-                        println!("drop: {s}");
-                    }
-                }
-            }
-            ui.table_next_column();
-        }
-    }*/
-
-    // we have to call next_row because we didn't make headers..
-    let display_size = ui.io().display_size;
-    let cell_size = display_size[0].min(display_size[1]) / 8. - 10.;
-    for i in 0..64 {
-        if i % 8 != 0 {
-            ui.same_line();
-        }
-        let _id = ui.push_id_usize(i);
-        ui.button_with_size("Btn", [cell_size, cell_size]);
-
-        if let Some(v) = ui.drag_drop_source_config("DRAG").begin_payload("Payload") {
-            ui.text("Sending text");
-            v.end()
-        }
-        if let Some(v) = ui.drag_drop_target() {
-            if let Some(p) = v.accept_payload("DRAG", imgui::DragDropFlags::empty()) {
-                if let Ok(v) = p {
-                    let s: &str = v.data;
-                    println!("drop: {s}");
-                }
-            }
-        }
-    }
-}
-
-fn draw_ui(ui: &imgui::Ui) {
-    
-    let window_token = ui.window("Chess")
-        .no_decoration()
-        .position([0., 0.], imgui::Condition::Always)
-        .size(ui.io().display_size, imgui::Condition::Always).begin();
-    if let Some(_t) = window_token {
-        draw_chess(ui);
-    }
-    /*for _ in 0..8 {
-        for _ in 0..8 {
-            ui.button_with_size("Button", [32f32, 32f32]);
-        }
-        ui.new_line();
-    }*/
-}
-
 fn main() {
-    /* initialize SDL and its video subsystem */
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
 
-    /* hint SDL to initialize an OpenGL 3.3 core profile context */
     let gl_attr = video_subsystem.gl_attr();
 
     gl_attr.set_context_version(3, 3);
     gl_attr.set_context_profile(GLProfile::Core);
 
-    /* create a new window, be sure to call opengl method on the builder when using glow! */
     let window = video_subsystem
         .window("GChess: Chess 4 real Gs", 720, 720)
         .allow_highdpi()
@@ -103,38 +162,45 @@ fn main() {
         .build()
         .unwrap();
 
-    /* create a new OpenGL context and make it current */
     let gl_context = window.gl_create_context().unwrap();
     window.gl_make_current(&gl_context).unwrap();
 
-    /* enable vsync to cap framerate */
     window.subsystem().gl_set_swap_interval(1).unwrap();
 
-    /* create new glow and imgui contexts */
     let gl = glow_context(&window);
 
-    /* create context */
     let mut imgui = Context::create();
 
-    /* disable creation of files on disc */
     imgui.set_ini_filename(None);
     imgui.set_log_filename(None);
 
-    /* setup platform and renderer, and fonts to imgui */
-    imgui
-        .fonts()
-        .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
+    let font = imgui.fonts().add_font(&[imgui::FontSource::TtfData {
+        data: include_bytes!("DejaVuSans.ttf"),
+        size_pixels: 48.,
+        config: Some(imgui::FontConfig {
+            glyph_ranges: imgui::FontGlyphRanges::from_slice(&[
+                1,      // Ascii
+                128,
+                0x265A, // Chess pieces in Unicode
+                0x265F,
+                0,
 
-    /* create platform and renderer */
+            ]),
+            ..Default::default()
+        }),
+    }]);
+
+    imgui.io_mut().config_flags |= imgui::ConfigFlags::NO_MOUSE_CURSOR_CHANGE;
+
     let mut platform = SdlPlatform::init(&mut imgui);
     let mut renderer = AutoRenderer::initialize(gl, &mut imgui).unwrap();
 
-    /* start main loop */
     let mut event_pump = sdl.event_pump().unwrap();
+
+    let mut game_state = GameState::new_game();
 
     'main: loop {
         for event in event_pump.poll_iter() {
-            /* pass all events to imgui platfrom */
             platform.handle_event(&mut imgui, &event);
 
             if let Event::Quit { .. } = event {
@@ -142,19 +208,23 @@ fn main() {
             }
         }
 
-        /* call prepare_frame before calling imgui.new_frame() */
         platform.prepare_frame(&mut imgui, &window, &event_pump);
 
         let ui = imgui.new_frame();
-        //ui.show_demo_window(&mut true);
-        draw_ui(ui);
+        {
+            let _font = ui.push_font(font);
+            let _no_padding = ui.push_style_var(imgui::StyleVar::WindowPadding([0., 0.]));
+            draw_ui(ui, &mut game_state);
+        }
 
-        /* render */
+
         let draw_data = imgui.render();
 
         unsafe { renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
+
         renderer.render(draw_data).unwrap();
 
         window.gl_swap_window();
+        std::thread::sleep(std::time::Duration::from_millis(20));
     }
 }
